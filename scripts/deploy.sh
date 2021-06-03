05a2c72a608ec25045fb68ffba818fdb9e22a0f8
ekene-ezedi <ezedi.eo.ekene@gmail.com>
2021-06-03 11:28:03 +0100

    push to heroku registry and deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
-echo "FROM DEPLOY"
-echo $1
+
 #login to heroku
-# echo $1 | docker login --username=_ --password-stdin registry.heroku.com
+echo $1 | docker login --username=_ --password-stdin registry.heroku.com
 
-# #tag docker image for heroku
-# docker tag notifyme-api registry.heroku.com/notifyme-api/web
+#tag docker image for heroku
+docker tag notifyme-api registry.heroku.com/notifyme-api/web
 
-# #push docker image to heroku registry
-# docker push registry.heroku.com/notifyme-api/web
+#push docker image to heroku registry
+docker push registry.heroku.com/notifyme-api/web
 
-# #release new container
-# heroku container:release web -a notifyme-api
-# © 2021 GitHub, Inc.
+#release new container
+heroku container:release web -a notifyme-api

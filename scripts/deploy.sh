df04f3dcf1c352597012de7bf7d31c90fc6c50ee
ekene-ezedi <ezedi.eo.ekene@gmail.com>
2021-06-03 21:04:38 +0100

    add password-stdin

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #login to heroku
-docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-# echo $1 | docker login --username=_ --password-stdin registry.heroku.com
+# docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+echo $HEROKU_API_KEY | docker login --username=_ --password-stdin registry.heroku.com
 
 #tag docker image for heroku
 docker tag notifyme-api registry.heroku.com/notifyme-api/web

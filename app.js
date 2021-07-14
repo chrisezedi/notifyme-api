6f107b8e3e77f3eec178dba4e035bf1497df513f
ekene-ezedi <ezedi.eo.ekene@gmail.com>
2021-07-14 12:44:59 +0100

    add new cors setup

--- app.js
+++ app.js
@@ -3,26 +3,35 @@ const express = require("express");
 const app = express();
 
 //cors config
-const whitelist = ['http://localhost:4200','https://notifyme-client.herokuapp.com']
-const corsOption = {
-  origin: function (origin, callback) {
-
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-  optionsSuccessStatus: 200
+const allowlist = ['http://localhost:4200','https://notifyme-client.herokuapp.com']
+let corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
 }
+// const corsOption = {
+//   origin: function (origin, callback) {
+
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   },
+//   allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
+//   methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+//   optionsSuccessStatus: 200
+// }
 
 if(app.get('env') == 'test'){
-    app.options('*',cors())
+    app.use(cors())
 }
 
-app.options('*',cors(corsOption))
+app.use(cors(corsOptionsDelegate))
 
 //connect to db
 require("./startup/db")();
